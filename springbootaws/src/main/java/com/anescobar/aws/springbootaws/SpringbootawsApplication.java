f10cdc71a0e798e680072e19928313d17a27ffb2
Antonio Escobar <aescobartizon@gmail.com>
2018-01-28 18:35:59 +0100

    AWS spring boot proyect

---
+++ springbootaws/src/main/java/com/anescobar/aws/springbootaws/SpringbootawsApplication.java
@@ -0,0 +1,12 @@
+package com.anescobar.aws.springbootaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootawsApplication.class, args);
+	}
+}
